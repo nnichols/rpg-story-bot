9c9dd70d818f1a51d53ded6b9517f88e3600d1f6
Nick A. Nichols <nichols1991@gmail.com>
2018-05-18 15:09:00 -0500

    Added extra tweet forms

--- src/rpg_story_bot/core.clj
+++ src/rpg_story_bot/core.clj
@@ -3,6 +3,11 @@
             [rpg-story-bot.twitter-interface :as twit])
   (:gen-class))
 
+(defn get-random-map-vector
+  "Draw a random inner member from a map of vectors"
+  [target-map]
+  (rand-nth (get target-map (rand-nth (keys targer-map)))))
+
 (defn -main
   "Create a message and tweet it out"
   [& args]
--- src/rpg_story_bot/forms.clj
+++ src/rpg_story_bot/forms.clj
@@ -1,6 +1,8 @@
 (ns rpg-story-bot.forms
-  (:require [story-elements.beings :as sb]
-            [story-elements.titles :as st])
+  (:require [story-elements.beings            :as sb]
+            [story-elements.titles            :as st]
+            [story-elements.player-attributes :as pa]
+            [rpg-story-bot.utility            :as util])
   (:gen-class))
 
 (defn since-running
@@ -11,3 +13,25 @@
        (rand-nth sb/humanoids)
        " attacked, we have been running from "
        (st/title-individual (rand-nth sb/humanoid-individuals))))
+
+(defn into-a-bar
+  "Generate a plot hook of the following form:
+  A __, ___, and ____ walk into a bar. Only one walks out"
+  []
+  (str "A "
+       (util/get-random-map-vector pa/races-map)
+       ", "
+       (util/get-random-map-vector pa/races-map)
+       ", and "
+       (util/get-random-map-vector pa/races-map)
+       " walk into a bar. Only one walks out."))
+
+(defn next-character
+  "Generate a random character"
+  []
+  (str "When my current character dies, I'm going to roll a "
+       (rand-nth pa/alignments)
+       " "
+       (util/get-random-map-vector pa/races-map)
+       " "
+       (util/get-random-map-vector pa/classes-map)))       
